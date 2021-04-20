4065b4d24fa7ea95fbef6d011e516c193c6fd9ea
romansuh <72558747+romansuh@users.noreply.github.com>
2021-04-20 21:53:41 +0300

    added fill to createdAt field into database for User and Habit (#6)

--- src/main/java/com/lyceum/habitapi/service/UserService.java
+++ src/main/java/com/lyceum/habitapi/service/UserService.java
@@ -5,6 +5,8 @@ import com.lyceum.habitapi.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+
 @Service
 public class UserService {
 
@@ -12,6 +14,7 @@ public class UserService {
     private UserRepository userRepository;
 
     public User addUser(User user) {
+        user.setCreatedAt(Instant.now());
         return userRepository.save(user);
     }
 }
