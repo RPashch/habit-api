7e3da6f1e926d91e557b10a45adbb502659f0ba5
RPashchenko <roman@bidmaze.co.uk>
2021-09-04 12:26:43 +0300

    fixed schema and added heroku plugin

--- src/main/java/com/lyceum/habitapi/HabitApiApplication.java
+++ src/main/java/com/lyceum/habitapi/HabitApiApplication.java
@@ -16,60 +16,60 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class HabitApiApplication {
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private CategoryRepository categoryRepository;
-
-    @Autowired
-    private HabitService habitService;
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private CategoryRepository categoryRepository;
+//
+//    @Autowired
+//    private HabitService habitService;
 
     public static void main(String[] args) {
         SpringApplication.run(HabitApiApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-//			String[] beanNames = ctx.getBeanDefinitionNames();
-//			Arrays.sort(beanNames);
-//			for (String beanName : beanNames) {
-//				System.out.println(beanName);
-//			}
-
-//            User user = User.builder()
-//                    .userName("keksik new")
-//                    .firstName("Vitalik")
-//                    .lastName("Fontanskiy")
+//    @Bean
+//    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//        return args -> {
+//
+////			String[] beanNames = ctx.getBeanDefinitionNames();
+////			Arrays.sort(beanNames);
+////			for (String beanName : beanNames) {
+////				System.out.println(beanName);
+////			}
+//
+////            User user = User.builder()
+////                    .userName("keksik new")
+////                    .firstName("Vitalik")
+////                    .lastName("Fontanskiy")
+////                    .build();
+////
+////            userRepository.save(user);
+//            Category category = Category.builder()
+//                    .description("description new category")
+//                    .title("sport")
+//                    .isDefault(false)
 //                    .build();
 //
-//            userRepository.save(user);
-            Category category = Category.builder()
-                    .description("description new category")
-                    .title("sport")
-                    .isDefault(false)
-                    .build();
-
-//            Long catId = categoryRepository.save(category)
-//                    .getId();
-
-            categoryRepository.save(category);
-
-//            Habit habit = Habit.builder()
-//                    .title("title")
-//                    .categoryId(categoryPersisted.getId())
-//                    .build();
+////            Long catId = categoryRepository.save(category)
+////                    .getId();
 //
-//            User persistedUser = userRepository.save(user);
+//            categoryRepository.save(category);
 //
-//            habitService.addHabit(habit, persistedUser);
-
-//			User userFromDb = userRepository.findByUserName("test");
-
-
-        };
-    }
+////            Habit habit = Habit.builder()
+////                    .title("title")
+////                    .categoryId(categoryPersisted.getId())
+////                    .build();
+////
+////            User persistedUser = userRepository.save(user);
+////
+////            habitService.addHabit(habit, persistedUser);
+//
+////			User userFromDb = userRepository.findByUserName("test");
+//
+//
+//        };
+//    }
 
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,43 +1,47 @@
-CREATE TABLE public.USER (
-    id serial PRIMARY KEY,
-    user_name VARCHAR(250),
-    first_name VARCHAR(250),
-    last_name VARCHAR(250),
-    birth_date DATE,
-    created_at TIMESTAMP DEFAULT timezone('utc', CURRENT_TIMESTAMP),
-    CONSTRAINT user_habit_user_id_fk
-    FOREIGN KEY (id)Ω
-        REFERENCES USER_HABIT (user_id)
-            ON DELETE CASCADE
+CREATE TABLE CATEGORY
+(
+    id          serial PRIMARY KEY,
+    title       VARCHAR(250) NOT NULL,
+    description VARCHAR(250),
+    is_default  boolean,
+    created_at  TIMESTAMP DEFAULT timezone('utc', CURRENT_TIMESTAMP)
+
+
 );
 
-CREATE TABLE HABIT (
-    id serial PRIMARY KEY,
-    title VARCHAR(250) NOT NULL,
+CREATE TABLE public.USER
+(
+    id         serial PRIMARY KEY,
+    user_name  VARCHAR(250),
+    first_name VARCHAR(250),
+    last_name  VARCHAR(250),
+    birth_date DATE,
+    created_at TIMESTAMP DEFAULT timezone('utc', CURRENT_TIMESTAMP)
+
+);
+
+CREATE TABLE HABIT
+(
+    id          serial PRIMARY KEY,
+    title       VARCHAR(250) NOT NULL,
     description VARCHAR(250),
     category_id int,
-    created_at TIMESTAMP DEFAULT timezone('utc', CURRENT_TIMESTAMP),
-    CONSTRAINT habit_user_habit_id_fk
-        FOREIGN KEY (id)
-            REFERENCES USER_HABIT (habit_id)
-                 ON DELETE CASCADE
+    created_at  TIMESTAMP DEFAULT timezone('utc', CURRENT_TIMESTAMP),
+    CONSTRAINT habit_category_category_id_fk
+        FOREIGN KEY (category_id)
+            REFERENCES CATEGORY (id)
+            ON DELETE SET NULL
 );
 
-CREATE TABLE CATEGORY (
-    id serial PRIMARY KEY,
-    title VARCHAR(250) NOT NULL,
-    description VARCHAR(250),
-	is_default boolean,
-    created_at TIMESTAMP DEFAULT timezone('utc', CURRENT_TIMESTAMP),
-    CONSTRAINT category_category_id_fk
-        FOREIGN KEY (id)
-            REFERENCES HABIT (category_id)
-                 ON DELETE SET NULL
-
-);
-
-CREATE TABLE USER_HABIT (
-    user_id int,
-    habit_id int,
-	CONSTRAINT user_habit_pk PRIMARY KEY (user_id, habit_id)
+CREATE TABLE USER_HABIT
+(
+    user_id  int not null
+        CONSTRAINT user_habit_user_id_fk
+            REFERENCES public.USER
+            ON DELETE CASCADE,
+    habit_id int not null
+        CONSTRAINT habit_user_habit_id_fk
+            REFERENCES HABIT
+            ON DELETE CASCADE,
+    CONSTRAINT user_habit_pk PRIMARY KEY (user_id, habit_id)
 );
